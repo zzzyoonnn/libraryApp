89616b4af56d18637aeebb5d6e2f275ac274c91d
zzzyoonnn <dwsrh03@gmail.com>
2024-05-04 22:43:43 +0900

    [Day 4] Database 조작하기(2)

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -2,13 +2,11 @@ package com.group.libraryapp.controller.user;
 
 import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -66,5 +64,37 @@ public class UserController {
 
         });
     }
+
+    @PutMapping("/user")
+    public void updateUser(@RequestBody UserUpdateRequest request) {
+        // id를 기준으로 유저가 존재하는지 확인하기 위해 SELECT 쿼리 작성
+        String readSql = "SELECT * FROM user WHERE id = ?";
+        // SQL을 날려 DB에 데이터가 있는지 확인
+        // jdbcTemplate.query()의 결과인 List가 비어 있다면, 유저 X
+        boolean isUserNotExitst = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, request.getId()).isEmpty();
+        if (isUserNotExitst) {                          // 만약 유저가 존재하지 않는다면
+            throw new IllegalArgumentException();       // IllegalArgumentException 던짐
+        }
+
+        String sql = "UPDATE user SET name = ? WHERE id = ?";
+        jdbcTemplate.update(sql, request.getName(), request.getId());
+    }
+
+    @DeleteMapping("/user")
+    public void deleteUser(@RequestParam String name) {     // 객체로 만들 수 있지만, 쿼리가 하나이기 때문에 RequestParam 사용
+        // name을 기준으로 유저가 존재하는지 확인하기 위해 SELECT 쿼리 작성
+        String readSql = "SELECT * FROM user WHERE name = ?";
+        // SQL을 날려 DB에 데이터가 있는지 확인
+        // jdbcTemplate.query()의 결과인 List가 비어 있다면, 유저 X
+        boolean isUserNotExitst = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, name).isEmpty();
+        if (isUserNotExitst) {                          // 만약 유저가 존재하지 않는다면
+            throw new IllegalArgumentException();       // IllegalArgumentException 던짐
+        }
+
+        String sql = "DELETE FROM user WHERE name = ?";
+        jdbcTemplate.update(sql, name);
+    }
+
+
 
 }
