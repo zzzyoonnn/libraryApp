c27dde5a58d75ea3d94d473d3bd51e490d0a91f7
zzzyoonnn <dwsrh03@gmail.com>
2024-05-13 15:52:07 +0900

    [Day 7] Spring Data JPA를 사용한 데이터베이스 조작(24강)

--- src/main/java/com/group/libraryapp/domain/user/User.java
+++ src/main/java/com/group/libraryapp/domain/user/User.java
@@ -1,9 +1,21 @@
 package com.group.libraryapp.domain.user;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
 
-    private  String name;
-    private Integer age;
+    @Id         // PRIMARY KEY. import할 때, javax.persistence로 import 하기
+    @GeneratedValue(strategy = GenerationType.IDENTITY)     // AUTO_INCREMENT
+    private Long id = null;     // bigint -> long
+
+    // User객체의 name에 적용
+    @Column(nullable = false, length = 20, name = "name")       // name varchar(20)
+    private String name;
+    private Integer age;        // 테이블과 동일하기 때문에 @Column 어노테이션 생략 가능
+
+    // JPA는 기본 생성자가 꼭 필요하다!
+    protected User() {}
 
     public User(String name, Integer age) {
         if (name == null || name.isBlank()) {
